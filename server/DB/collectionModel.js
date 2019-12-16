5dacfd1f93a7145df0c46431531e8dd2030b39e3
Roger Wei <45575275+yyywrz@users.noreply.github.com>
2019-12-16 15:11:08 +0800

    update data

--- server/DB/collectionModel.js
+++ server/DB/collectionModel.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-const model = new mongoose.Schema({});
+const exchangeSchema = require('./currencySchema')
+const model = new mongoose.Schema(exchangeSchema);
 const dataModel = mongoose.model('weatherByCity', model,'weatherByCity')
 
-mongoose.connect('mongodb://127.0.0.1:27017/weatherDatabase',{ useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/currency_database',{ useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
